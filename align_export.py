f4578099940de0124f0f8c62f5575fbee6b118fa
rtviii <rtkushner@gmail.com>
2020-06-24 14:41:32 -0700

    tuple arg-parsing on the subchain-alignmnt script, execute independently of pymol\chimerax sesh

--- align_export.py
+++ align_export.py
@@ -1,5 +1,20 @@
 import ast
 from pymol import cmd, selector
+from argparse import ArgumentParser
+
+def modelChainTuple(string):
+    try:
+        model,chain=map(str, string.split(','))
+        return model,chain
+    except:
+        print("Type error when reading model-chain tuples")
+        raise TypeError
+
+parser = ArgumentParser(description='Specify models.chains to align or extend pymol api',)
+parser.add_argument('--model-chain-tuples', nargs='+', type=modelChainTuple)
+parser.add_argument('--pmlxtnd')
+args =parser.parse_args()
+print(args)
 
 """
 -fetch the structures(arg) 
@@ -7,7 +22,8 @@ from pymol import cmd, selector
 -align in the same coordinate frame
 -save as a pdb
 """
-def chain_align_save(*args, **kwargs):
+
+def chain_align_save(*args, **kwargs): 
     args = [ast.literal_eval(kvpair) for kvpair in args]
     for pair in args:
         cmd.fetch(str.lower(pair[0]))
@@ -15,14 +31,10 @@ def chain_align_save(*args, **kwargs):
         cmd.delete(str.lower(pair[0]))
     
     for mobile in [ '{}.{}'.format(model, chain) for (model, chain) in args ][1:]:
-        # cmd.align(mobile, "{}.{}".format(args[0][0], args[0][1]))
         cmd.super(mobile, "{}.{}".format(args[0][0], args[0][1]),reset=1,transform=1,quiet=0)
 
     cmd.reset()
-
     cmd.save('alignment.cif')
-    
-
     
 
 def create_subchain_object(pdbid, subchain):
@@ -31,8 +43,19 @@ def create_subchain_object(pdbid, subchain):
     cmd.create('{}.{}'.format(pdbid,subchain), tempname)
     cmd.delete(tempname)
 
+if args.pmlxtnd:
+    cmd.extend('create_subchain_object', create_subchain_object)
+    cmd.extend('chain_align_save', chain_align_save);
+    print("Added two commands to pymol.cmd")
+else:
+    mcpairs=args.model_chain_tuples
+    for pair in mcpairs:
+        cmd.fetch(str.lower(pair[0]))
+        create_subchain_object(pair[0], pair[1])
+        cmd.delete(str.lower(pair[0]))
+    
+    for mobile in [ '{}.{}'.format(model, chain) for (model, chain) in mcpairs ][1:]:
+        cmd.super(mobile, "{}.{}".format(mcpairs[0][0], mcpairs[0][1]),reset=1,transform=1,quiet=0)
 
-cmd.extend('create_subchain_object', create_subchain_object)
-cmd.extend('chain_align_save', chain_align_save);
-
-
+    cmd.reset()
+    cmd.save('alignment.cif')
--- extracting-alignment-matrices.md
+++
@@ -1,2 +0,0 @@
-```get_view``` yields the state matrix
-https://pymol.org/dokuwiki/doku.php?id=api:cmd:alpha
